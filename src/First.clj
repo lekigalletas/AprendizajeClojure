35b5f237d7b8b68ad5d9af1fd62d68abf4c7dac3
ander <ander.lekanda11@gmail.com>
2025-01-13 12:22:05 +0100

    Primeros Ejercicios

--- src/First.clj
+++ src/First.clj
@@ -1,1 +1,14 @@
 (ns First)
+
+
+;;EJERCICIOS DE LISTAS Y PARECIDOS PARA APRENDER
+
+;; crear una lista
+(def my-list (list 1 2 3 4 5))
+
+;;Usando Range
+(def my-list2 (range 1 11))
+
+;;leer la lista
+(println my-list)
+(println my-list2)
